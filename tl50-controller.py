0a87f718aeb66f12e2d40a7ce28d58c329abc988
Mike Clement <mikec@evercharge.com>
2022-12-12 17:44:21 -0800

    Refactor

--- tl50-controller.py
+++ tl50-controller.py
@@ -1,14 +1,74 @@
+class Color(object):
+    NONE = 0x00
+    GREEN = 0x00
+    RED = 0x01
+    ORANGE = 0x02
+    AMBER = 0x03
+    YELLOW = 0x04
+    LIME_GREEN = 0x05
+    SPRING_GREEN = 0x06
+    CYAN = 0x07
+    SKY_BLUE = 0x08
+    BLUE = 0x09
+    VIOLET = 0x0a
+    MAGENTA = 0x0b
+    ROSE = 0x0c
+    WHITE = 0x0d
 
-import serial
-import binascii
-import time
-from optparse import OptionParser
 
-cmd_checksum = [0x00, 0x00]
+class Intensity(object):
+    NONE = 0x00
+    HIGH = 0x00
+    LOW = 0x01
+    MEDIUM = 0x02
+    OFF = 0x03
+
+
+class Animation(object):
+    NONE = 0x00
+    OFF = 0x00
+    STEADY = 0x01
+    FLASH = 0x02
+    TWO_COLOR_FLASH = 0x03
+    HALF_HALF = 0x04
+    HALF_HALF_ROTATE = 0x05
+    CHASE = 0x06
+    INTENSITY_SWEEP = 0x07
+
+
+class Speed(object):
+    NONE = 0x00
+    STANDARD = 0x00
+    FAST = 0x01
+    SLOW = 0x02
+
+
+class Pattern(object):
+    NONE = 0x00
+    NORMAL = 0x00
+    STROBE = 0x01
+    THREE_PULSE = 0x02
+    SOS = 0x03
+    RANDOM = 0x04
+
+
+class Rotation(object):
+    NONE = 0x00
+    COUNTER_CLOCKWISE = 0x00
+    CLOCKWISE = 0x01
+
+
+class Audible(object):
+    NONE = 0x00
+    OFF = 0x00
+    STEADY = 0x01
+    PULSED = 0x02
+    SOS = 0x03
+
 
 def compute_checksum(commands):
     """
-    compute_checksum calcuates the correct checksum based on the Banner Engineering serial protocol documentation
+    compute_checksum calculates the correct checksum based on the Banner Engineering serial protocol documentation
     
     returns: a list with two hex bytes
     """
@@ -16,127 +76,101 @@ def compute_checksum(commands):
     command_total = sum(commands)
     ones_comp = command_total ^ 0xFFFF
     
-    byte1 = (ones_comp & 0xff00) >>8
+    byte1 = (ones_comp & 0xff00) >> 8
     byte2 = (ones_comp & 0x00ff)
-    #print(hex(byte1) + ':'+hex(byte2))
-    return [byte2, byte1] 
+    return [byte2, byte1]
 
 
-def set_segment(**kwargs):
+def build_command_bytearray(color1, intensity1, animation, speed, pattern, color2, intensity2, rotation, audible):
     """
-    set_segment creates a 30 byte hex command to send to the TL50 light via a USB serial using the following minimum inputs:
+    build_command_bytearray creates a 38 byte command to send to the TL50 light via a serial protocol. Documentation
+    can be found here: https://info.bannerengineering.com/cs/groups/public/documents/literature/218025.pdf
     
-    - intensity = see color_intensity_index
-    - color_num_one = see colors_index
-    - color_num_two = see colors_index
-    - animation = see animation_index
-    
-    returns (default): a binary formatted hex string of 30 bytes, including a checksum.
-    returns (debug mode): a binary formatted hex string of 30 bytes, including a checksum, but using a static debug_string list of hex bytes instead of a generated one.
-    
+    returns: a bytearray of length 38 bytes, including a checksum.
+
     Note:
     for the Chase animation: color #2 is the background, color #1 is the moving color 
     """
-    command_string = [0xF4, 0x41, 0xC1, 0x1F, 0x00, 0x0d, 0x00, 0xd, 0x00, 0x00, 0x00, 0x00, 0x00, 00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
-    
-    debug_string =   [0xF4, 0x41, 0xC1, 0x1F, 0x00, 0x00, 0x05, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
-    
-    animation_index = {
-      "off": 0x00,
-      "steady": 0x01,
-      "flash": 0x02,
-      "two_color_flash": 0x03,
-      "half_half":0x04,
-      "half_half_rot": 0x05,
-      "chase": 0x06,
-      "intens_sweep": 0x07
-    }
-    colors_index = {
-      "green": 0x00,
-      "red": 0x01,
-      "orange": 0x02,
-      "amber": 0x03,
-      "yellow":0x04,
-      "lime_green": 0x05,
-      "spring_green": 0x06,
-      "cyan": 0x07,
-      "sky_blue": 0x08,
-      "blue": 0x09,
-      "violet":0xa,
-      "magenta":0xb,
-      "rose": 0xc,
-      "white": 0xd
-    }
-    color_intensity_index = {
-      "high": 0x00,
-      "low": 0x01,
-      "medium": 0x02,
-      "off": 0x03,
-    }
-    
-    color_inten = color_intensity_index.get(kwargs['intensity'], 0x00)
-        
-    color_one = colors_index.get(kwargs['color_num_one'], 0xd)
-    color_two = colors_index.get(kwargs['color_num_two'], 0xd)
-    
-    command_string[5] = color_one #color one 
-    command_string[7] = color_two #color two
-    
-    animation_type = animation_index.get(kwargs['animation'], 0x00)
-    #set the 6th byte to the correct hex string 
-    command_string[6] = animation_type
-    
-    #generate the array
-    if options.debug == True:
-        light_command = bytearray( debug_string + compute_checksum(debug_string) )
-    else:
-        light_command = bytearray( command_string + compute_checksum(command_string) )
-    
-    if options.debug == True:
-        print("Args are the following:")
-        print(kwargs)
-        #print("Byte Command")
-        #print(light_command)
-        print("Light Number:")
-        print(color_num)
-        
-    return light_command  
 
-#CLI Parsing Init
-parser = OptionParser()
-parser.add_option("--debug", action="store_true", dest="debug")
+    # The first five bytes are hard-coded
+    command_string = [0xF4, 0x41, 0xC1, 0x1F, 0x00] + 31 * [0x00]
 
-(options, args) = parser.parse_args() #create a varible to store the state of the debug command
-   
-#Create a new serial object and set the baudrate and port   
-ser = serial.Serial()
-ser.baudrate = 19200
-#ser.write_timeout=2
-#ser.inter_byte_timeout=2
+    # The sixth byte is a bitfield
+    # 1-4: color code 1
+    # 5-7: intensity code 1
+    # 8: reserved as 0
+    command_string[5] = (color1 & 0xf) | ((intensity1 & 0x7) << 4)
 
-ser1 = serial.Serial()
-ser1.baudrate = 19200
+    # The seventh byte is a bitfield
+    # 1-3: animation code
+    # 4-5: speed code
+    # 6-8: pattern code
+    command_string[6] = (animation & 0x7) | ((speed & 0x3) << 3) | ((pattern & 0x7) << 5)
 
-#single hight frosted
-ser.port = '/dev/tty.usbserial-FT791OX9'
+    # The eighth byte is a bitfield
+    # 1-4: color code 2
+    # 5-7: intensity code 2
+    # 8: rotation direction code
+    command_string[7] = (color2 & 0xf) | ((intensity2 & 0x7) << 4) | ((rotation & 0x1) << 7)
 
-#double hight frosted
-#ser.port = '/dev/tty.usbserial-FT7BVSDW'
+    # The ninth through 35th bytes are 0x00
 
-#clear light
-#ser1.port= '/dev/tty.usbserial-FT4VLXSZ'
-    
-ser.open()
-#ser1.open()
+    # The 36th byte is an audible code
+    command_string[35] = audible
 
-if ser.is_open:    
-    #ser.write(set_segment(color_num="1", intensity="high", color="green", animation="intens_sweep"))
-    #ser1.write(set_segment(color_num="1", intensity="high", color="green", animation="flash"))
-    #time.sleep(0.09)
-    #time.sleep(5)
-    
-    ser.write(set_segment(intensity="high", color_num_one="rose", color_num_two="blue", animation="two_color_flash"))
-    time.sleep(0.09)
-    #time.sleep(5)
-    
-    ser.close()
+    # The 37th and 38th bytes are the checksum
+    checksum = compute_checksum(command_string)
+
+    return bytearray(command_string + checksum)
+
+
+def off():
+    return build_command_bytearray(Color.NONE, Intensity.OFF, Animation.NONE, Speed.NONE, Pattern.NONE, Color.NONE,
+                                   Intensity.NONE, Rotation.NONE, Audible.NONE)
+
+def steady(color, intensity):
+    return build_command_bytearray(color, intensity, Animation.STEADY, Speed.NONE, Pattern.NONE, Color.NONE,
+                                   Intensity.NONE, Rotation.NONE, Audible.NONE)
+
+def flash(color, intensity, speed, pattern):
+    return build_command_bytearray(color, intensity, Animation.FLASH, speed, pattern, Color.NONE,
+                                   Intensity.NONE, Rotation.NONE, Audible.NONE)
+
+def two_color_flash(color1, intensity1, color2, intensity2, speed, pattern):
+    return build_command_bytearray(color1, intensity1, Animation.TWO_COLOR_FLASH, speed, pattern, color2,
+                                   intensity2, Rotation.NONE, Audible.NONE)
+
+def half_half(color1, intensity1, color2, intensity2):
+    return build_command_bytearray(color1, intensity1, Animation.HALF_HALF, Speed.NONE, Pattern.NONE, color2,
+                                   intensity2, Rotation.NONE, Audible.NONE)
+
+def half_half_rotate(color1, intensity1, color2, intensity2, speed, rotation):
+    return build_command_bytearray(color1, intensity1, Animation.HALF_HALF_ROTATE, speed, Pattern.NONE, color2,
+                                   intensity2, rotation, Audible.NONE)
+
+def chase(color1, intensity1, color2, intensity2, speed, rotation):
+    return build_command_bytearray(color1, intensity1, Animation.CHASE, speed, Pattern.NONE, color2,
+                                   intensity2, rotation, Audible.NONE)
+
+
+if __name__ == '__main__':
+    import serial
+    import time
+
+    ser = serial.Serial()
+    ser.baudrate = 19200
+    ser.port = '/dev/ttyUSB0'
+
+    with ser as s:
+        def change(cmd, secs):
+            s.write(cmd)
+            s.flush()
+            time.sleep(secs)
+
+        change(steady(Color.GREEN, Intensity.MEDIUM), 2)
+        change(flash(Color.AMBER, Intensity.HIGH, Speed.STANDARD, Pattern.STROBE), 2)
+        change(two_color_flash(Color.YELLOW, Intensity.HIGH, Color.BLUE, Intensity.HIGH, Speed.FAST, Pattern.NORMAL), 2)
+        change(half_half(Color.SPRING_GREEN, Intensity.HIGH, Color.MAGENTA, Intensity.HIGH), 2)
+        change(half_half_rotate(Color.SPRING_GREEN, Intensity.HIGH, Color.MAGENTA, Intensity.HIGH, Speed.FAST, Rotation.CLOCKWISE), 2)
+        change(chase(Color.SPRING_GREEN, Intensity.HIGH, Color.MAGENTA, Intensity.HIGH, Speed.SLOW, Rotation.COUNTER_CLOCKWISE), 2)
+        change(off(), 0)
